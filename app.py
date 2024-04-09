a4729aad5176ac50a75b433172fa89bc626d4b86
paqj <changraven@gmail.com>
2024-04-09 23:55:23 +0900

    .

--- app.py
+++ app.py
@@ -1,12 +1,8 @@
 import streamlit as st
 
-st.title("Input Your OpenAI API Key on Side Bar")
+st.title("Build a SiteGPT version for Cloudflare's documentation.")
 
 with st.sidebar:
-    # st.title("Input Your OpenAI API Key on Side Bar")
-    # 사용자로부터 OpenAI API 키 입력 받기
-    # user_api_key = st.text_input("Enter your OpenAI API key", "")
-
     st.markdown(
         """
         [GitHub Repository](https://github.com/paqj/vs-gpt-assign7)
